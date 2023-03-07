ac16f0dbc04882e9ef5838d5b85286ed14068e64
Thap310 <61404862+Thap310@users.noreply.github.com>
2023-03-07 13:40:24 +0200

    Header Design

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm:'480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'primary': '#29ABE2',
+      'dark-blue': '#004A66',
+      'light-blue': '#F2FCFF',
+      'dark': '#002938',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
